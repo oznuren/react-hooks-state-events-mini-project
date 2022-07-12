ecd87603ae4f67b75a00f3d4902ce5f6e2624469
oznuren <95101906+oznuren@users.noreply.github.com>
2022-07-12 14:25:28 +0300

    Completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, currCategory, setCurrCategory }) {
+  const handleCategory = (e) => {
+    setCurrCategory(e.target.value);
+  };
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => {
+        return (
+          <button
+            key={category}
+            value={category}
+            className={currCategory === category ? "selected" : ""}
+            onClick={handleCategory}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, setTasks }) {
+  function handleDelete(event) {
+    setTasks((tasks) =>
+      tasks.filter((task) => {
+        return task.text !== event.target.value;
+      })
+    );
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => {
+        return (
+          <Task
+            key={task.text}
+            text={task.text}
+            category={task.category}
+            handleDelete={handleDelete}
+          />
+        );
+      })}
     </div>
   );
 }
